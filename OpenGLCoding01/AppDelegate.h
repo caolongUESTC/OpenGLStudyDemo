abbbc17c0782c0c0dc1be19fdb1789f03e9e0877
caolong <caolong.bj02@bytedance.com>
2020-07-22 11:43:16 +0800

    Initial Commit

---
+++ OpenGLCoding01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLCoding01
+//
+//  Created by 曹龙 on 2020/7/22.
+//  Copyright © 2020 曹龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLCoding01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLCoding01
+//
+//  Created by 曹龙 on 2020/7/22.
+//  Copyright © 2020 曹龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
